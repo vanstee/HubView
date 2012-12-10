91536eec3c7628c3aa4592191e6777fe2b18699a
vanstee <vanstee@highgroove.com>
2012-12-10 01:04:57 -0500

    Setup AFNetworking

---
+++ HubView/AFGitHubClient.h
@@ -0,0 +1,13 @@
+//
+//  AFGitHubClient.h
+//  HubView
+//
+//  Created by Patrick Van Stee on 12/9/12.
+//  Copyright (c) 2012 Patrick Van Stee. All rights reserved.
+//
+
+#import "AFHTTPClient.h"
+
+@interface AFGitHubClient : AFHTTPClient
+
+@end
---
+++ HubView/AFGitHubClient.m
@@ -0,0 +1,13 @@
+//
+//  AFGitHubClient.m
+//  HubView
+//
+//  Created by Patrick Van Stee on 12/9/12.
+//  Copyright (c) 2012 Patrick Van Stee. All rights reserved.
+//
+
+#import "AFGitHubClient.h"
+
+@implementation AFGitHubClient
+
+@end
